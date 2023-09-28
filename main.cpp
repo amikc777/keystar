2ba5e041e4df4834046a8ec8575d3f2207faba2e
Amik <amikc1996@gmail.com>
2023-09-28 12:04:20 -0400

    Refactored the grandstaffrenderer to a generic renderer class. Tested around with the new Note class. Decided to leave the main implementation for the Note class for later on.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,69 @@
 #include <iostream>
 #include <raylib.h>
 
-class GrandStaffRenderer { 
-    public:
-        GrandStaffRenderer(const char* imagePath) {
-           grandStaffTexture = LoadTexture(imagePath);
-        }
-        ~GrandStaffRenderer(){
-            UnloadTexture(grandStaffTexture);
-        }
+// Hold off on the Note class now, and just get a Note rendered on the grand staff.
 
-        void Draw() {
-            DrawTexture(grandStaffTexture, 0, 0, WHITE);
-        }
+/*class Note {
+public:
+    Vector2 position;  // Position of the note on the screen
+
+    Note(float x, float y) : position{ x, y } {}
+
+    // void Render() const {
+    //     // Draw a circle for the note at the specified position
+    //     DrawCircleV(position, 20, RED);
+    // }
+
+    // Set the position of the note
+    void SetPosition(float x, float y) {
+        position.x = x;
+        position.y = y;
+    }
+};
+*/
+class Renderer {
+public:
+    Renderer(const char* imagePath) {
+        Texture = LoadTexture(imagePath);
+    }
+
+    ~Renderer() {
+        UnloadTexture(Texture);
+    }
+
+    void Draw(int x, int y) {
+        DrawTexture(Texture, x, y, WHITE);
+    }
+
 private:
-    Texture2D grandStaffTexture;
+    Texture2D Texture;
 };
 
-int main(){
+int main() {
     const int screenWidth = 800;
-    const int screenHeight = 600; 
+    const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "KeyStar: The Guitar-Hero Style Piano App");
 
-    // Reminder to insert the filepath of the asset being used.
-    GrandStaffRenderer grandStaffRenderer("GrandStaff.jpg"); 
+    Renderer grandStaffRenderer("assets//grandstaff.png");
+    Renderer noteTexture("assets//testnote.png");
+
+    // Note testNote(0, 0);
+    // testNote.SetPosition(150, 200); // Adjust position based on your requirement
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        grandStaffRenderer.Draw(); // Image of the Grand Staff gets drawn.
+        ClearBackground(WHITE);
+
+        grandStaffRenderer.Draw(0, 0);  // Render the grand staff
+        noteTexture.Draw(150, 200);
+        // testNote.Render();  // Render the test note
+
         EndDrawing();
     }
+
     CloseWindow();
+
+    return 0;
 }
